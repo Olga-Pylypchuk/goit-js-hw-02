23f9af911ce67a83b8ae0b06e664938b546313bf
Olga-Pylypchuk <ollaamm87@gmail.com>
2025-01-28 22:16:31 +0200

    added tasks 1,4

---
+++ js/task-2.js
@@ -0,0 +1,24 @@
+'use strict';
+
+// const minutes = 0;
+// const hours = 12;
+// const time = minutes > 0 ? `${hours} h. ${minutes} m.` : `${hours} h.`;
+// console.log(time);
+
+// const deadline = 7;
+
+// if(deadline === 1){
+// console.log('Today');
+
+// }
+// else if(deadline === 2){
+// console.log('Tomorrow');
+
+// }
+// else if(deadline === 3){
+//     console.log('Overmorrow');
+    
+// }
+// else {
+//     console.log('DAte in the future');
+//     }
